8ac8d32c41125e1d82aea0c24289cf5506335b72
ZENBY\Aryan <fe.aryan@outlook.com>
2022-10-08 16:43:05 +0530

    new#

---
+++ new2.py
@@ -0,0 +1,23 @@
+import numbers
+
+
+numbers =  int(input("Enter any number from 1-7 :-"))
+
+
+
+if numbers == 1:
+    print("Sunday")
+elif numbers == 2:
+    print("Monday")
+elif numbers == 3:
+    print("Tuesday")
+elif numbers == 4:
+    print("Wendesday")
+elif numbers == 5:
+    print("Thursday")
+elif numbers == 6:
+    print("Friday")
+elif numbers == 7:
+    print("Saturday")
+else:
+    print("Enter a valid numbers")                        
