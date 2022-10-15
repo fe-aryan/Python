557377be59b747c359030b5767817a74d9d47076
ZENBY\Aryan <fe.aryan@outlook.com>
2022-10-15 16:33:52 +0530

    case

--- strings.py
+++ strings.py
@@ -17,3 +17,15 @@ print (a.lower())
 print(a.strip())
 print(a.replace("H", "Y"))
 print(a.replace("IT'S" , " "))
+
+print("This is an example of slash n \n Hello there \n Namasteyyy")
+
+#take a string of length 5 [index 0-4]
+#print first char
+#print middle char
+#print last char
+
+a = "Files"
+print(a[0])
+print(a[2])
+print(a[4])
