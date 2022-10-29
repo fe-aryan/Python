eb5d4cff7cc7de29459e122278842110e5aef796
fe-aryan <fe.aryan@outlook.com>
2022-10-29 13:46:31 +0530

    fun

--- kaat/loop.py
+++ kaat/loop.py
@@ -1,23 +1,23 @@
 #While loop
-a= 1
+a = 1
 
 while a < 10:
     print(a)
-    a = a+ 1
+    a = a + 1
 
-a=1
-while a<10:
-    a=a+1
+a = 1
+while a < 10:
+    a = a + 1
 
-    if a ==5 :
+    if a == 5:
         continue
     print(a)
 
 
-a=1
-while a<10:
-    a=a+1
+a = 1
+while a < 10:
+    a = a + 1
 
-    if a ==5 :
+    if a == 5:
         break
     print(a)
