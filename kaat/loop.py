b5af1a75e7cf9147cddf064d78bb74c009e9d0cb
fe-aryan <fe.aryan@outlook.com>
2022-10-29 13:44:19 +0530

    fun

---
+++ kaat/loop.py
@@ -0,0 +1,23 @@
+#While loop
+a= 1
+
+while a < 10:
+    print(a)
+    a = a+ 1
+
+a=1
+while a<10:
+    a=a+1
+
+    if a ==5 :
+        continue
+    print(a)
+
+
+a=1
+while a<10:
+    a=a+1
+
+    if a ==5 :
+        break
+    print(a)
