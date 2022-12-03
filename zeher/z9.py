0aa950ad3a899297e2b619c32f9cf1c466367848
fe-aryan <fe.aryan@outlook.com>
2022-12-03 12:37:38 +0530

    patched

--- zeher/z9.py
+++ zeher/z9.py
@@ -1,3 +1,19 @@
 tuple1 = (1,[6,7],2,3,4,5)
 i = tuple1[1][0]
 print(i)
+
+# •	Sets is a non-homogenous but stores in single variable
+# •	Representation= {}
+# •	no Duplicate element 
+# •	unchangeable
+#   unordered
+set={"cars","boat","bike"}
+print(set)
+#no Duplicate element 
+set1={"cars","boat","bike","cars"}
+print(set1)
+print(len(set))
+if "bike" in set:
+    print("True")
+else:
+  print("False")
